dff49fca0870a70a02ed3eb52b9b99c2cd37d8f9
arl18-08 <arl18@email.sc.edu>
2026-09-22 14:16:21 -0400

    b- B

--- day7/script.js
+++ day7/script.js
@@ -4,8 +4,8 @@ const damageAmount = 30;
 function takeDamage() {
     playerHealth = playerHealth - damageAmount;
 
-    const healthText = document.getElementbyId("health-display");
-    const statusText = document.getElementbyId("status-message");
+    const healthText = document.getElementById("health-display");
+    const statusText = document.getElementById("status-message");
 
     healthText.innerText = playerHealth;
 
