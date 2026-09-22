d6db48e44e0a8c77d9400ab99bb97c18c48cda1e
arl18-08 <arl18@email.sc.edu>
2026-09-22 14:02:43 -0400

    added onclick

--- day7/script.js
+++ day7/script.js
@@ -1,6 +1,11 @@
 let playerHealth = 100;
 const damageAmount = 30;
 
-function takeDamage(){
-    playerHealth= playerHealth- damageAmount;
+function takeDamage() {
+    playerHealth = playerHealth - damageAmount;
+
+    const healthText = document.getElementbyId("health-display");
+    const statusText = document.getElementbyId("status-message");
+
+    healthText.innerText = playerHealth;
 }
