c9988db9a34688fb3de152e00724caf3fb0b20b4
arl18-08 <arl18@email.sc.edu>
2026-09-24 13:38:56 -0400

    intinal commit of javas

--- lab5/script.js
+++ lab5/script.js
@@ -1,27 +1,51 @@
-let playerHealth = 100;
-const damageAmount = 30;
+let moneyBalance = 100;
+const healthAmount = 10;
+const damageAmount = 10;
 
-function takeDamage() {
-    playerHealth = playerHealth - damageAmount;
+function addHealth() {
+    moneyBalance = moneyBalance - healthAmount;
 
     const healthText = document.getElementById("health-display");
     const statusText = document.getElementById("status-message");
 
-    healthText.innerText = playerHealth;
+    healthText.innerText= moneyBalance;
 
-    if(playerHealth > 0) 
+}
+
+
+function takeDamage() {
+    moneyBalance = moneyBalance - damageAmount;
+
+    const healthText = document.getElementById("health-display");
+    const statusText = document.getElementById("status-message");
+
+    healthText.innerText = moneyBalance;
+
+    if(moneyBalance> 0) 
     {
-        healthText.innerText = playerHealth;
-        statusText.innerText = "you've been hit!";
+        healthText.innerText = moneyBalance;
+        statusText.innerText = "you got money!";
     }
     else
     {
         healthText.innerText = 0;
-        statusText.innerText = "game over.";
+        statusText.innerText = "you're broke.";
         statusText.style.color = "#a52c1f";
         statusText.style.fontWeight = "bold";
         document.body.style.backgroundColor ="#5a1a1a";
         document.querySelector("button").disabled = true;
-        document.querySelector("button").innerText = "DEAD";
+        document.querySelector("button").innerText = "BROKE";
     }
+
+    if(moneybalance> 200)
+    {
+        healthText.innerText = 200;
+        statusText.innerText = "greed is a sin.";
+        statusText.style.color = "#a52c1f";
+        statusText.style.fontWeight = "bold";
+        document.body.style.backgroundColor ="#5a1a1a";
+        document.querySelector("button").disabled = true;
+        document.querySelector("button").innerText = "GREEDY";
+    }
+
 }
